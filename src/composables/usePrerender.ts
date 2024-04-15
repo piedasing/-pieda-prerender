7116aaa9eeab8ca4d8db87c386e52f8aeb2bd776
pieda <pieda@coder.com.tw>
2024-04-15 21:27:43 +0800

    init project

---
+++ src/composables/usePrerender.ts
@@ -0,0 +1,72 @@
+type TUsePrerender = {
+    staticDir: string;
+    outputDir: string;
+    indexPath: string;
+    port: number;
+    routes: string[];
+    injectData?: { [x: string]: any };
+};
+
+export const usePrerender = ({
+    staticDir,
+    outputDir,
+    indexPath,
+    port = 8080,
+    routes,
+    injectData = {},
+}: TUsePrerender) => {
+    return {
+        closeBundle: async () => {
+            const fs: any = await importModule('fs');
+            const path: any = await importModule('path');
+            const Prerenderer: any = await importModule('@prerenderer/prerenderer');
+            const PuppeteerRenderer: any = await importModule('@prerenderer/renderer-puppeteer');
+            const prerenderer = new Prerenderer({
+                staticDir,
+                indexPath,
+                server: {
+                    port,
+                },
+                renderer: new PuppeteerRenderer({
+                    injectProperty: '__PRERENDER_INJECTED',
+                    inject: {
+                        IS_PERRENDER: true,
+                        ...injectData,
+                    },
+                    headless: false,
+                    renderAfterDocumentEvent: 'prerender-event',
+                    viewport: {
+                        width: 1920,
+                        height: 1080,
+                    },
+                }),
+            });
+
+            return prerenderer
+                .initialize()
+                .then(() => {
+                    return prerenderer.renderRoutes(routes);
+                })
+                .then((renderedRoutes: any[]) => {
+                    renderedRoutes.forEach((renderedRoute) => {
+                        fs.writeFileSync(
+                            path.join(outputDir, 'index.html'),
+                            renderedRoute.html.trim(),
+                        );
+                    });
+                })
+                .catch((error: Error) => {
+                    throw error;
+                })
+                .finally(() => {
+                    return prerenderer.destroy();
+                });
+        },
+    };
+};
+
+function importModule(moduleName: string) {
+    return new Promise((resolve, reject) => {
+        import(moduleName).then((loadedModule) => resolve(loadedModule.default)).catch(reject);
+    });
+}
